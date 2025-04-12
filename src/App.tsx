bb622c90267f5829fb7b71a3bda17312a5c0d4a8
Bartłomiej Ostojski <worldofworld0@gmail.com>
2025-04-12 15:28:18 +0200

    feat: add navigation bar to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import Home from '@/pages/home.tsx';
 import MaterialsManagementWrapper from '@/modules/materials-management/materials-management-wrapper.tsx';
+import MaterialsWrapper from '@/modules/materials-management/materials/materials-wrapper.tsx';
+import CreateMaterialForm from '@/modules/materials-management/materials/pages/form/create-material-form.tsx';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/wizard-form" element={<MaterialsManagementWrapper />} />
-      </Routes>
+      <nav>
+        <ul className="flex justify-center gap-4 font-semibold py-2 shadow-md">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/materials-management/materials/create">Create Material</Link>
+          </li>
+        </ul>
+      </nav>
+      <main className="container mx-auto py-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/materials-management" element={<MaterialsManagementWrapper />}>
+            <Route path="materials" element={<MaterialsWrapper />}>
+              <Route path="create" element={<CreateMaterialForm />} />
+            </Route>
+          </Route>
+        </Routes>
+      </main>
     </div>
   );
 }
