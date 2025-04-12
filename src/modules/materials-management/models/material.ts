330c84336ba684dbfde7d9316c3941d99461a94e
Bartłomiej Ostojski <worldofworld0@gmail.com>
2025-04-12 21:41:07 +0200

    fix: make optional fields optional

--- src/modules/materials-management/models/material.ts
+++ src/modules/materials-management/models/material.ts
@@ -4,11 +4,11 @@ import { UnitOfMeasure } from '@/types/unit-of-measure.enum.ts';
 const material = z.object({
     uuid: z.string().uuid(),
     number: z.string().min(1).max(12),
-    description: z.string().max(255),
-    sku: z.string().max(18),
-    price: z.number().positive(),
-    unit_of_measure: z.nativeEnum(UnitOfMeasure),
-    weight: z.number().positive(),
+    description: z.string().max(255).optional(),
+    sku: z.string().max(18).optional(),
+    price: z.number().positive().optional(),
+    unit_of_measure: z.nativeEnum(UnitOfMeasure).optional(),
+    weight: z.number().positive().optional(),
 });
 
 type Material = z.infer<typeof material>;
