f1c911c5a32f6c795db3f4b96070d2d1cc3fd79d
Bartłomiej Ostojski <worldofworld0@gmail.com>
2025-04-13 11:11:33 +0200

    feat: add tabs to create form

--- src/modules/materials-management/materials/pages/form/create/create-material-form.tsx
+++ src/modules/materials-management/materials/pages/form/create/create-material-form.tsx
@@ -1,10 +1,32 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs.tsx';
 
 const CreateMaterialForm = () => {
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+
     return (
         <div className="flex flex-col">
             <h1 className="text-2xl">Tworzenie materiału</h1>
             <div className="flex flex-col mt-8">
+                <Tabs value={pathname} className="w-full mb-4">
+                    <TabsList>
+                        <TabsTrigger
+                            value="/materials-management/materials/create"
+                            onClick={() => navigate('/materials-management/materials/create')}
+                        >
+                            Detale
+                        </TabsTrigger>
+                        <TabsTrigger
+                            value="/materials-management/materials/create/characteristics"
+                            onClick={() =>
+                                navigate('/materials-management/materials/create/characteristics')
+                            }
+                        >
+                            Charakterystyki
+                        </TabsTrigger>
+                    </TabsList>
+                </Tabs>
                 <Outlet />
             </div>
         </div>
